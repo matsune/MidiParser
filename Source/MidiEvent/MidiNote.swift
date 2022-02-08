f84082adb63c2cc38ceaf5d9d7c0ac8940322254
live77 <live77>
2022-02-08 23:22:23 +0800

    make regular timestamp and regular duration public

--- Source/MidiEvent/MidiNote.swift
+++ Source/MidiEvent/MidiNote.swift
@@ -11,8 +11,8 @@ import Foundation
     
 public struct MidiNote {
     
-    private let regularTempoTimeStamp: MusicTimeStamp
-    private let regularDuration: Float32
+    public let regularTempoTimeStamp: MusicTimeStamp
+    public let regularDuration: Float32
     
     public let timeStamp: MidiTime
     public let duration: MidiTime
